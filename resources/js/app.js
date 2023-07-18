8fbf3b177dbd1c78f8b2c8d46312d0b3a667900b
Vlad <vladek2000@inbox.ru>
2023-07-18 14:35:04 +0400

    sail container

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import Index from "./components/Index.vue";
+
+const app = createApp({});
+
+app.component('index', Index)
+
+app.mount('#app')
